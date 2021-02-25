edac02319e6f30edb73a6fd769ee527e38b2afb2
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-02-25 09:21:22 -0300

    Alteração na base

--- Aula01/prog1/OlaMundo.java
+++ Aula01/prog1/OlaMundo.java
@@ -1,8 +1,10 @@
-package Aula01.prog1;
 class OlaMundo{
     public static void main(String[] args) {
         //Isso é um comentário de uma linha
         System.out.println("Ola Mundo!");
+        System.out.println("Stein é mal?");
+        System.out.println("Overwatch morreu?");
+        System.out.println("Aqui!");
         /*
         * Comentário de diversas linhas
         * Continuação do comentário
