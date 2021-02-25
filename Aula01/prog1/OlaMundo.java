a712e7afeead1bb8b50847056ccb4b420820e8a2
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-02-25 08:57:01 -0300

    Codando e compilando na mão

---
+++ Aula01/prog1/OlaMundo.java
@@ -0,0 +1,11 @@
+package Aula01.prog1;
+class OlaMundo{
+    public static void main(String[] args) {
+        //Isso é um comentário de uma linha
+        System.out.println("Ola Mundo!");
+        /*
+        * Comentário de diversas linhas
+        * Continuação do comentário
+        */ 
+    }
+}
