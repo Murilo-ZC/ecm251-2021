0e20a180f4351d1b84beff4d00f0f5806ae4cee0
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-03-15 08:57:17 -0300

    Operação sacar()

--- Aula05/src/carvalho/zanini/Sistema.java
+++ Aula05/src/carvalho/zanini/Sistema.java
@@ -25,6 +25,15 @@ public class Sistema {
             case 1:
                 System.out.println("Saldo Atual: R$" + this.conta.getSaldo());
                 break;
+            case 2:
+                System.out.println("Qual quantia de dinheiro?");
+                double quantiaParaSacar = scanner.nextDouble();
+                if (this.conta.sacar(quantiaParaSacar)){
+                    System.out.println("Operação Realizada com Sucesso!");
+                } else {
+                    System.out.println("Não foi possível realizar a operação!");
+                }
+                break;
             default:
                 System.out.println("Essa funcionalidade ainda não foi implementada");
         }
