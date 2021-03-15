ba0128bc09ebb216c6a2abf449581ff0d9809a90
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-03-15 09:28:55 -0300

    Titulo de pagamento

--- Aula05/src/carvalho/zanini/Sistema.java
+++ Aula05/src/carvalho/zanini/Sistema.java
@@ -34,6 +34,27 @@ public class Sistema {
                     System.out.println("Não foi possível realizar a operação!");
                 }
                 break;
+            case 3:
+                System.out.println("Qual quantia de dinheiro?");
+                double quantiaParaDepositar = scanner.nextDouble();
+                this.conta.depositar(quantiaParaDepositar);
+                System.out.println("Operação Realizada com Sucesso!");
+                break;
+            case 5:
+                System.out.println("Nome da Conta:");
+                String nome = scanner.next();
+                System.out.println("Valor da conta:");
+                double valorDaConta = scanner.nextDouble();
+                System.out.println("Juros da conta:");
+                double jurosDaConta = scanner.nextDouble();
+                System.out.println("Informe a data de vencimento (ano-mes-dia)?");
+                String dataVencimentoTitulo = scanner.next();
+                Titulo titulo = new Titulo(valorDaConta,nome,dataVencimentoTitulo, jurosDaConta);
+                if(this.conta.sacar(titulo.totalParaPagar())){
+                    System.out.println("Operação Realizada com Sucesso!");
+                } else {
+                    System.out.println("Algo de errado aconteceu!");
+                }
             default:
                 System.out.println("Essa funcionalidade ainda não foi implementada");
         }
