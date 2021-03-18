ea718dd8959cd41fe4ff75e19eeeed59c0f445f8
Murilo-ZC <carvalho.zanini@gmail.com>
2021-03-18 09:12:01 -0300

    Validador de CPF e atributos e métodos da classe

---
+++ aula05-teoria/src/carvalho/zanini/Main.java
@@ -0,0 +1,9 @@
+package carvalho.zanini;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String cpf = "12345678912";
+        System.out.println("Cpf valido:" + ValidadorCPF.validarCpf(cpf));
+    }
+}
