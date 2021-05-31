111a97e90c52766ab5b8f6a4fac706559cb4bd11
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-05-31 08:58:37 -0300

    Java Collections Parte 2

---
+++ Aula14/src/br/maua/maps/HashMapTestDrive.java
@@ -0,0 +1,34 @@
+package br.maua.maps;
+
+import br.maua.models.Item;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapTestDrive {
+    public static void main(String[] args) {
+        Map<String, Item> itemMap = new HashMap<>();
+
+        //Adiciona itens no mapa
+        itemMap.put("Item1", new Item("Maca",1));
+        itemMap.put("Item2", new Item("Pera",2));
+        itemMap.put("Item3", new Item("Maca",1));
+        itemMap.put("Item1", new Item("Banana",3));
+
+        //Passa por todos os elementos
+        itemMap.forEach((chave, valor) -> System.out.println("Chave:"+chave+"\tValor:"+valor));
+
+        //Imprime todos os elementos do map
+        System.out.println(itemMap);
+
+        //Outra forma de recuperar os itens do Mapa
+        Item item = itemMap.get("Item1");
+        System.out.println(item);
+        item = itemMap.get("Goku");
+        System.out.println(item);
+        item = itemMap.getOrDefault("Vegeta", new Item("Ola", 100));
+        System.out.println(item);
+        item = itemMap.getOrDefault("Item2", new Item("Ola", 100));
+        System.out.println(item);
+    }
+}
